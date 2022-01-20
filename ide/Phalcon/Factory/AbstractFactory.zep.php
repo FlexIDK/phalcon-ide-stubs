104832816268ac1a3af8221632e3278bfd3f5ceb
eugene <eugene@krivoruchko.info>
2022-01-20 21:51:11 +0300

    4.1.2

---
+++ ide/Phalcon/Factory/AbstractFactory.zep.php
@@ -0,0 +1,73 @@
+<?php
+
+/* This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+namespace Phalcon\Factory;
+
+use Phalcon\Config;
+use Phalcon\Config\ConfigInterface;
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+abstract class AbstractFactory
+{
+    /**
+     * @var array
+     */
+    protected $mapper = [];
+
+    /**
+     * @var array
+     */
+    protected $services = [];
+
+
+    /**
+     * Checks the config if it is a valid object
+     *
+     * @param mixed $config
+     * @return array
+     */
+    protected function checkConfig($config): array
+    {
+    }
+
+    /**
+     * Returns the adapters for the factory
+     *
+     * @return array
+     */
+    abstract protected function getAdapters(): array;
+
+    /**
+     * Checks if a service exists and throws an exception
+     *
+     * @param string $name
+     * @return mixed
+     */
+    protected function getService(string $name)
+    {
+    }
+
+    /**
+     * AdapterFactory constructor.
+     *
+     * @param array $services
+     * @return void
+     */
+    protected function init(array $services = []): void
+    {
+    }
+
+}
---
+++ ide/Phalcon/Session/Bag.zep.php
@@ -0,0 +1,111 @@
+<?php
+
+/* This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+namespace Phalcon\Session;
+
+use Phalcon\Collection;
+use Phalcon\Di;
+use Phalcon\Di\DiInterface;
+use Phalcon\Di\InjectionAwareInterface;
+
+/**
+ * Phalcon\Session\Bag
+ *
+ * This component helps to separate session data into "namespaces". Working by
+ * this way you can easily create groups of session variables into the
+ * application
+ *
+ * ```php
+ * $user = new \Phalcon\Session\Bag("user");
+ *
+ * $user->name = "Kimbra Johnson";
+ * $user->age  = 22;
+ * ```
+ */
+class Bag extends Collection implements \Phalcon\Di\InjectionAwareInterface
+{
+
+    private $container;
+
+
+    private $name = null;
+
+
+    private $session;
+
+
+    /**
+     * Phalcon\Session\Bag constructor
+     *
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+    }
+
+    /**
+     * Destroys the session bag
+     *
+     * @return void
+     */
+    public function clear(): void
+    {
+    }
+
+    /**
+     * Returns the DependencyInjector container
+     *
+     * @return DiInterface
+     */
+    public function getDI(): DiInterface
+    {
+    }
+
+    /**
+     * Initialize internal array
+     *
+     * @param array $data
+     * @return void
+     */
+    public function init(array $data = []): void
+    {
+    }
+
+    /**
+     * Removes a property from the internal bag
+     *
+     * @param string $element
+     * @return void
+     */
+    public function remove(string $element): void
+    {
+    }
+
+    /**
+     * Sets a value in the session bag
+     *
+     * @param string $element
+     * @param mixed $value
+     * @return void
+     */
+    public function set(string $element, $value): void
+    {
+    }
+
+    /**
+     * Sets the DependencyInjector container
+     *
+     * @param \Phalcon\Di\DiInterface $container
+     * @return void
+     */
+    public function setDI(\Phalcon\Di\DiInterface $container): void
+    {
+    }
+
+}
